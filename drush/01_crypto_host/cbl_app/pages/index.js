135538bad92120fbf3654ec5edfe7b21ff783f27
Oleksii <1653234+alexmazaltov@users.noreply.github.com>
2024-10-10 14:26:50 +0300

    cbl_app Homepage

---
+++ drush/01_crypto_host/cbl_app/pages/index.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Dashboard from '../components/Dashboard';
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <Dashboard />
+            <Footer />
+        </div>
+    );
+}
